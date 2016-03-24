6f8816b40babe216c075c380811560532915256d
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-24 21:24:30 +0800

    add number method

--- Testnumber.java
+++ Testnumber.java
@@ -5,7 +5,23 @@ class Testnumber{
 	public static void main(String[] args) {
 
 		number umber=new number();
+		umber.AaddtoList();
+		System.out.println("");
+		umber.BAaddtoList();
+
 		
+		umber.aBigaddtoList();
+		
+		umber.bBigaddtoList();
+
+		umber.sizesame();
+		umber.printa();
+		System.out.println("");
+		umber.printb();
+		umber.add();
+		System.out.println("");
+		umber.addprint();
+
 		
 	}
 }
