c8bbf26bf15daaa5d6bc39faaba6d756a33b7b53
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-03-25 14:21:10 +0800

    add method

--- number.java
+++ number.java
@@ -1,37 +1,39 @@
+//u10316052
 import java.util.*;
 public class number{
 	
 	String a;
 	String b;
 
-	ArrayList<Integer> aBig=new ArrayList<>();
-	ArrayList<Integer> bBig=new ArrayList<>();
+	ArrayList<Integer> aBig=new ArrayList<>();//int
+	ArrayList<Integer> bBig=new ArrayList<>();//int
+	ArrayList<Integer> addBig=new ArrayList<>();//intsum
+	ArrayList<String> aSBig=new ArrayList<>();//String judgment wheater.
+	ArrayList<String> bSBig=new ArrayList<>();//String judgment wheater
 
-	ArrayList<String> aSBig=new ArrayList<>();
-	ArrayList<String> bSBig=new ArrayList<>();
-
-	ArrayList<Integer> adouble=new ArrayList<>();
-	ArrayList<Integer> bdouble=new ArrayList<>();
+	ArrayList<Integer> adouble=new ArrayList<>();//.double
+	ArrayList<Integer> bdouble=new ArrayList<>();//.double
 	ArrayList<Integer> adddouble=new ArrayList<>();
-
+	//input constrtor
 	number(){
 		Scanner input=new Scanner(System.in);
-		System.out.print("請輸入兩數");
+		System.out.print("請輸入兩數，讓兩數加總");
 		a=input.nextLine();
 		b=input.nextLine();
-		AaddtoList();
-		BAaddtoList();
 	}
+	//add to list
 	public void AaddtoList(){
 		for(int i=a.length()-1;i>=0;i--){
 			aSBig.add(String.valueOf(a.charAt(i)));
 		}
 	}
+	//add to list
 	public void BAaddtoList(){
 		for(int i=a.length()-1;i>=0;i--){
 			bSBig.add(bString.valueOf(.charAt(i)));
 		}
 	}
+	//judment double
 	public boolean Ajudgmentdouble(){
 		return aSBig.contains(".");
 	}
@@ -39,13 +41,14 @@ public class number{
 	public boolean Bjudgmentdouble(){
 		return bSBig.contains(".");
 	}
+	//judment double
 	public int BjudgmentdoubleB(){
 		if(Bjudgmentdouble())
 		return bSBig.indexOf(".");
 		else
 		return 0;
 	}
-
+	//judment double
 	public int AjudgmentdoubleA(){
 		if(Ajudgmentdouble())
 		return aSBig.indexOf(".");
@@ -53,26 +56,43 @@ public class number{
 		return 0;
 	}
 	//add to intlist
-	public void aBigaddtoList(){	
-		for(int i=0;i<=aSBig.size()-1;i++){
-			aBig.add(Integer.parseInt(aSBig.get(i)));
+	public void aBigaddtoList(){
+		if(Ajudgmentdouble()){
+			for(int i=AjudgmentdoubleA()+1;i<=aSBig.size()-1;i++){
+				aBig.add(Integer.parseInt(aSBig.get(i)));
+			}
+		}else{	
+			for(int i=0;i<=aSBig.size()-1;i++){
+				aBig.add(Integer.parseInt(aSBig.get(i)));
+			}
 		}		
 	}
 	public void bBigaddtoList(){	
-		for(int i=0;i<=bSBig.size()-1;i++){
-			bBig.add(Integer.parseInt(bSBig.get(i)));
+	//add to intlist
+	public void bBigaddtoList(){
+		if(Ajudgmentdouble()){
+			for(int i=BjudgmentdoubleB()+1;i<=bSBig.size()-1;i++){
+				bBig.add(Integer.parseInt(bSBig.get(i)));
+			}
+		}else{			
+			for(int i=0;i<=bSBig.size()-1;i++){
+				bBig.add(Integer.parseInt(bSBig.get(i)));
+			}
 		}		
 	}
+	//printa
 	public void printa(){		
 		for(int ia = aBig.size()-1;ia >= 0; ia--){
 			System.out.print(aBig.get(ia));
 		}	
-	}	
+	}
+	//printb
 	public void printb(){
 		for(int ia = bBig.size()-1;ia >= 0; ia--){
 			System.out.print(bBig.get(ia));
 		}
 	}
+	//let same size
 	public void sizesame(){
 		while(bBig.size()!=aBig.size()){
 			if(bBig.size()>aBig.size())
@@ -81,9 +101,19 @@ public class number{
 				bBig.add(0);			
 		}		
 	}
+	//int add
 	public void add(){
 		int carry=0;
-		for(int i=0;i<=bBig.size()-1;i++){		
+		int sum=aBig.get(0)+bBig.get(0)+doubleadd();
+			
+		if(sum>=10){
+			addBig.add(sum-10);
+			carry=1;			
+		}else{ 
+			addBig.add(sum);
+			carry=0;
+		}
+		for(int i=1;i<=bBig.size()-1;i++){		
 			int sum=aBig.get(i)+bBig.get(i);
 			
 			if(sum>=10){
@@ -102,17 +132,18 @@ public class number{
 			System.out.print(addBig.get(ia));
 		}
 	}
+	//addtodoublea
 	public void doubleaddA(){
 		for(int i=0;i<=AjudgmentdoubleA()-1;i++)
 		adouble.add(Integer.parseInt(aSBig.get(i)));
 
 	}
-
+	//add to double blist
 	public void doubleaddB(){
 		for(int i=0;i<=BjudgmentdoubleB()-1;i++)
 		bdouble.add(Integer.parseInt(bSBig.get(i)));
 	}
-
+	//doublesamesize
 	public void sizesamedouble(){
 		while(adouble.size()!=bdouble.size()){
 			if(bdouble.size()>adouble.size())
@@ -121,8 +152,8 @@ public class number{
 				bdouble.add(0);			
 		}		
 	}
-
-	public void doubleadd(){
+	//doubleadd return carry to int
+	public int doubleadd(){
 		int carry=0;
 		for(int i=0;i<=adouble.size()-1;i++){		
 			int sum=adouble.get(i)+bdouble.get(i);
@@ -136,10 +167,13 @@ public class number{
 			}
 		}
 		if(carry==1)
-			adddouble.add(1);
+			return 1;
+		else
+			return 0;
 	}
+	//print sum
 	public void adddoubleprint(){
-		for(int ia = adddouble.size()-1;ia >= 0; ia--){
+		for(int ia = (adddouble.size()-1)/2;ia >= 0; ia--){
 			System.out.print(adddouble.get(ia));
 		}
 	}
